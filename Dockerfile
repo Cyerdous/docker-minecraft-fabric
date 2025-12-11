66e79162971c1c566a9e5aa173b21411879de4fd
Nora <nora@cyer.dev>
2025-12-10 23:58:25 -0500

    Initial working version

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,16 @@ FROM alpine:3.23.0
 
 RUN sed -i '2s/^# *//' /etc/apk/repositories
 
+RUN apk add bash
 RUN apk add openjdk21
+RUN apk add curl
 
 EXPOSE 25565
 
+COPY ./resources /
+
+RUN chmod +x /run.sh
+
 VOLUME ["/config"]
 
-ENTRYPOINT ["./run.sh"]
+ENTRYPOINT ["/run.sh"]
--- resources/run.sh
+++ resources/run.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+if [ "${dedicated_ram:-0}" -eq 0 ]; then
+	echo "ERROR: No amount of RAM added"
+	exit 1
+fi
+
+if [ "${eula:-0}" != "true" ]; then
+	echo "ERROR: You must agree to the eula"
+	exit 1
+fi
+
 # use fabric installer to get most recent minecraft version
-readarray -t lines < <(java -jar ./resources/fabric-installer-1.1.0.jar help)
+readarray -t lines < <(java -jar /fabric-installer-1.1.0.jar help)
 for line in "${lines[@]}"; do
 	if [[ $line =~ "Latest Version:" ]]; then
 		## set mcversion to most recent stable minecraft version
@@ -25,9 +35,17 @@ if [ "${version_fabric:-null}" != null ]; then
 	LOADERVERSION=$version_fabric
 fi
 
+# enable agreeing to the eula from docker run or compose
+if [ ! -f eula.txt ]
+then
+    echo "eula=${eula}" > /eula.txt
+fi
+
 # if "fabric-server-mc.{mcversion=$version_minecraft}-loader.{loaderversion=$version_fabric}-launcher.{launcherversion=1.1.0}.jar does not exist"
 if [ ! -f "fabric-server-mc.${MCVERSION}-loader.${LOADERVERSION}-launcher.1.1.0.jar" ]; then
 	curl -OJ https://meta.fabricmc.net/v2/versions/loader/${MCVERSION}/${LOADERVERSION}/1.1.0/server/jar
 fi
+
+wait
 
 java -Xmx${dedicated_ram}G -jar fabric-server-mc.${MCVERSION}-loader.${LOADERVERSION}-launcher.1.1.0.jar nogui
